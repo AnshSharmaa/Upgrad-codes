960c7e76a2960387bcb214b1715253f3eb51b083
Siddharth Sharma <65253410+03sid-sharma@users.noreply.github.com>
2021-01-17 18:55:01 +0530

    little change in if conditions
    Try running
    Input:
    3
    Cat
    Tree
    Bag
    
    Solution output
    Cat
    Bag
    Tree

--- Search-sort/quick sort string.java
+++ Search-sort/quick sort string.java
@@ -1,48 +1,51 @@
 import java.util.*;
 
- class Source {
-
-   public static void quickSort(String array[], int left, int right) {
-       if (left < right) {
-           int position = part(array, left, right);
-           quickSort(array, left, position - 1);
-           quickSort(array, position + 1, right);
-       }
-        
-   }
-    public static int part(String arr[], int l, int h) 
-    { 
-        int piv = arr[h].length();  
-        int i = (l-1);
-        for (int j=l; j<h; j++) 
-        { 
-            if (arr[j].length() < piv) 
-            { 
-                i++; 
-                String temp = arr[i]; 
-                arr[i] = arr[j]; 
-                arr[j] = temp; 
-            } 
-        } 
-        String temp = arr[i+1]; 
-        arr[i+1] = arr[h]; 
-        arr[h] = temp; 
-        return i+1; 
-    } 
-    public static void main(String args[]){
+public class Source {
+    public static void main(String[] args) {
         Scanner s=new Scanner(System.in);
         int n=s.nextInt();
         String[] x=new String[n];
         for(int i=0;i<n;i++){
             x[i]=s.next();
-           //System.out.println(x[i]+ "*");
         }
-        /*for(int i=0;i<x.length;i++){
-            System.out.println(x[i]+ "*");
-        }*/
-        quickSort(x,0,x.length-1);
+        QSorter quickSort = new QSorter();
+        quickSort.sort(x);
         for(int i=0;i<x.length;i++){
             System.out.println(x[i]);
         }
+    }
+}
+
+class QSorter {
+    public void sort(String[] arr) {
+        sort(arr, 0, arr.length - 1);
+    }
+    
+    private void sort(String[] arr, int start, int end) {
+        // Base Condition
+        if (start >= end)
+            return;
+        // Partition
+        int boundary = partition(arr, start, end);
+        // Sort Left
+        sort(arr, start, boundary - 1);
+        // Sort Right
+        sort(arr, boundary + 1, end);
+    }
+    
+    private int partition(String[] arr, int start, int end) {
+        int pivot = arr[end].length();
+        int boundary = start - 1;
+        for (int i = start; i <= end; i++)
+            if (arr[i].length() <= pivot)
+                swap(arr, i, ++boundary);
+
+        return boundary;
+    }
+
+    private void swap(String[] array, int index1, int index2) {
+        String temp = array[index1];
+        array[index1] = array[index2];
+        array[index2] = temp;
     }
 }
