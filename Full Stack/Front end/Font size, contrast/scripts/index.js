ad81de481218a7e06c52e438d8c92835f4468c03
Ansh Sharma <49720994+AnshSharmaa@users.noreply.github.com>
2021-03-06 22:03:51 +0530

    Assignment front end

---
+++ Full Stack/Front end/Font size, contrast/scripts/index.js
@@ -0,0 +1,26 @@
+// Good luck!
+//Add function names and change the variable name
+var fax=40;
+function {
+    obj=document.getElementById('content');
+    fax=fax+1;
+    obj.style.fontSize=(fax)+'px';
+}
+
+function {
+    obj=document.getElementById('content');
+    fax=fax-1;
+    obj.style.fontSize=(fax)+'px';
+}
+
+function {
+    obj=document.getElementById('content');
+    obj.style.background="#000";
+    obj.style.color="#fff";
+}
+
+function {
+    obj=document.getElementById('content');
+    obj.style.background="#fff";
+    obj.style.color="#000";
+}
